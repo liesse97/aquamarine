cdc8e3f991aa1988ed331768b2a0ba00ee476ed8
liesse97 <ishimwelieish@gmail.com>
2021-01-26 11:46:05 +0100

    FAQ javascript kind of works

--- FAQ/FAQ.js
+++ FAQ/FAQ.js
@@ -1,19 +1,28 @@
 const questions= document.querySelectorAll(".question");
+const arrow = document.querySelectorAll("arrow_click");
+const textAnswer= document.querySelectorAll(".answer");
 
 questions.forEach(item =>{
     item.addEventListener('click',openFaq)
 })
 
 function openFaq(e){
+   
     if (e.target.classList.contains('que_btn')){
         e.target.classList.toggle('display')
     }
 
     if(e.target.classList.contains('display')){
-        e.target.nextElementSibling.style.maxHeight='none';
-        e.target.querySelector('span').innerHTML= '<i class="fas fa-angle-up"></i>'
+         e.target.nextElementSibling.style.display = 'none';
+  
+        e.target.querySelector('span').innerHTML= '<i class="fas fa-angle-up"></i>';
     } else{
-            e.target.nextElementSibling.style.maxHeight='0';
-        e.target.querySelector('span').innerHTML= '<i class="fas fa-angle-down"></i>'
-    }
+       
+             e.target.nextElementSibling.style.display = 'block';
+        e.target.querySelector('span').innerHTML= '<i class="fas fa-angle-down"></i>';
+        
 }
+}
+
+
+
